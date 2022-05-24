d83cf9054d8bf534e33c898907cbf13ff5db4b0c
Ludwigson Morisseau <morisseau@Ludwigsons-MacBook-Air.local>
2022-05-24 16:56:53 -0400

    added Hero and count buttons

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,23 +1,31 @@
+import { useState } from 'react'
 import { View, Text, Button, StyleSheet } from 'react-native';
 
 export default function Counter() {
+    const [count, setCount] = useState(0);
     return (
         <View styles={styles.counterContainer}>
-            <Text> You Clicked the button 0 times</Text>
-            <Button title='Click Me' />
+            <Text style={styles.counterText}> You Clicked the button {count} times</Text>
+            <Button title='+' onPress={() => setCount(count + 1)}/>
+            <Button title='reset' onPress={() => setCount(0)}/>
+            <Button title='-' onPress={() => setCount(count - 1)}/>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
+const styles= StyleSheet.create({
     counterContainer: {
-        width: '80vw',
-        margin: '14',
-        padding: '14',
-        backgroundColor: 'beige',
+        width: '80%',
+        margin: 14,
+        padding: 14,
+        backgroundColor: 'red',
     },
     counterText: {
         fontSize: 30,
         textAlign: 'center',
+    },
+    plusButton: {
+        fontSize: 25,
+        fontWeight: 15,
     }
 })
