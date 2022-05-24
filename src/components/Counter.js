9bd9d5f56c9ebc878afdeb21d31190c021216122
Ludwigson Morisseau <morisseau@Ludwigsons-MacBook-Air.local>
2022-05-24 17:08:22 -0400

    corrected the counterContainer

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,9 +4,9 @@ import { View, Text, Button, StyleSheet } from 'react-native';
 export default function Counter() {
     const [count, setCount] = useState(0);
     return (
-        <View styles={styles.counterContainer}>
+        <View style={styles.counterContainer}>
             <Text style={styles.counterText}> You Clicked the button {count} times</Text>
-            <Button title='+' onPress={() => setCount(count + 1)}/>
+            <Button style={styles.plusButton} title='+' onPress={() => setCount(count + 1)}/>
             <Button title='reset' onPress={() => setCount(0)}/>
             <Button title='-' onPress={() => setCount(count - 1)}/>
         </View>
@@ -18,7 +18,8 @@ const styles= StyleSheet.create({
         width: '80%',
         margin: 14,
         padding: 14,
-        backgroundColor: 'red',
+        backgroundColor: '#fff',
+        borderRadius: 10,
     },
     counterText: {
         fontSize: 30,
